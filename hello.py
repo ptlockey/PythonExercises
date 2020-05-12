556e6806c8d36ac69107fdfccbc9746112216ad4
Philip Lockey <philiplockey@philips-mbp.home>
2020-05-12 17:46:05 +0100

    name.py from the pyhton crash course

---
+++ hello.py
@@ -0,0 +1,10 @@
+#This program says hello and asks for my name.
+print('hello world')
+print('what is your name?')
+myname=input()
+print('it is good to meet you, ' + myname)
+print('The length of your name is:')
+print(len(myname))
+print('what is your age')
+myage=input()
+print('you will be ' + str(int(myage) +1)+ ' in a year. )
